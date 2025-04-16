71a3fd157c71bfb3da7d0a9721d04bd12a11a291
fk <krueger.fab@gmail.com>
2025-04-16 16:48:35 +0200

    Add DataVerifier

---
+++ src/main/java/io/promptics/jobagent/verification/DataVerifier.java
@@ -0,0 +1,71 @@
+package io.promptics.jobagent.verification;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.promptics.jobagent.careerdata.CareerDataManager;
+import io.promptics.jobagent.careerdata.CareerDataMongoDbTools;
+import io.promptics.jobagent.careerdata.CareerDataMongoService;
+import io.promptics.jobagent.careerdata.model.CareerData;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataVerifier {
+
+    public static final String RENDER_MODEL = "gpt-3.5-turbo";
+    private final CareerDataMongoService careerDataMongoService;
+    private final ChatClient renderClient;
+    private final CareerDataManager careerDataManager;
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public DataVerifier(ChatClient.Builder builder, CareerDataMongoService careerDataMongoService, CareerDataManager careerDataManager) {
+        this.renderClient = builder.defaultOptions(ChatOptions.builder()
+                .model(RENDER_MODEL)
+                .temperature(0.0).build()).build();
+        this.careerDataMongoService = careerDataMongoService;
+        this.careerDataManager = careerDataManager;
+    }
+
+    public String execute(String userInput, String careerDataId) {
+
+        String content = "here's your career data: \n\n";
+
+        // User provided a message and probably wants to change data
+        if(userInput != null && !userInput.isEmpty()) {
+            content = careerDataManager.run(userInput, careerDataId);
+            try {
+                content = objectMapper.readTree(content).get("message").asText();
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        CareerData careerData = careerDataMongoService.getById(careerDataId);
+        String currentCareerData = renderCareerData(careerData);
+
+        return content + "\n" + currentCareerData;
+    }
+
+    private String renderCareerData(CareerData careerData) {
+        try {
+            // TODO: Remove rendering by model with code
+            String json = objectMapper.writeValueAsString(careerData);
+            String rendered = renderClient.prompt()
+                    .user("""
+                            Render the given JSON as {target}.
+                            
+                            Given JSON: {json}
+                            
+                            Expected output:
+                            Only return the plain Markdown. No ``` or any other additional text or markup.
+                            Do not return technical IDs.
+                            """.replace("{json}", json).replace("{target}", "Plain text"))
+                    .call()
+                    .content();
+            return rendered;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/io/promptics/jobagent/verification/DataVerifierTest.java
@@ -0,0 +1,65 @@
+package io.promptics.jobagent.verification;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.promptics.jobagent.careerdata.model.CareerData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Testcontainers
+class DataVerifierTest {
+
+    @Container
+    @ServiceConnection
+    final static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+    private String id;
+
+    @Autowired
+    private DataVerifier dataVerifier;
+
+    @BeforeEach
+    void beforeEach() {
+        try {
+            CareerData careerData = new ObjectMapper().readValue(new ClassPathResource("career-data.json").getFile(), CareerData.class);
+            CareerData saved = mongoTemplate.save(careerData);
+            id = saved.getId();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("should render career data")
+    void shouldRenderCareerData() {
+        String response = dataVerifier.execute(null, id);
+        System.out.println(response);
+        assertThat(response).contains("TechGiant");
+    }
+    
+    @Test
+    @DisplayName("should change company name")
+    void shouldChangeCompanyName() {
+        String userInput = "Please change the company name 'TechGiant' to 'TechDwarf'.";
+        String response = dataVerifier.execute(userInput, id);
+        System.out.println(response);
+        CareerData careerData = mongoTemplate.findById(id, CareerData.class);
+        assertThat(careerData.getWork().get(2).getName()).isEqualTo("TechDwarf");
+    }
+
+}
