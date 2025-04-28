b7761dde948ed6a941530fda9365a684e98b59bc
fk <krueger.fab@gmail.com>
2025-04-28 21:15:10 +0200

    Remove api key check

--- src/main/java/io/promptics/jobagent/InterviewApplication.java
+++ src/main/java/io/promptics/jobagent/InterviewApplication.java
@@ -8,17 +8,10 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class InterviewApplication implements ApplicationListener<ApplicationReadyEvent> {
+public class InterviewApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InterviewApplication.class, args);
     }
 
-    @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
-        String openaiApiKey = System.getenv("SPRING_AI_OPENAI_API_KEY");
-        if(openaiApiKey == null) {
-            throw new IllegalStateException("No API key found. Please provide as env 'SPRING_AI_OPENAI_API_KEY'");
-        }
-    }
 }
