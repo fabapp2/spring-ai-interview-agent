14cfb7ccb31e92ad574079f117ba3cfb143b0bdd
fk <krueger.fab@gmail.com>
2025-04-28 10:25:59 +0200

    Extract OpenAiKeyVerifier from Application class

---
+++ src/main/java/io/promptics/jobagent/OpenAiApiKeyVerifier.java
@@ -0,0 +1,22 @@
+package io.promptics.jobagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenAiApiKeyVerifier  implements ApplicationListener<ApplicationReadyEvent> {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewApplication.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        String openaiApiKey = System.getenv("SPRING_AI_OPENAI_API_KEY");
+        if(openaiApiKey == null) {
+            throw new IllegalStateException("No API key found. Please provide as env 'SPRING_AI_OPENAI_API_KEY'");
+        }
+    }
+}
