5c863878b67dc789a1061b96d85cc58d603ed5c9
fk <krueger.fab@gmail.com>
2025-04-29 22:59:35 +0200

    Add assertion

--- src/test/java/io/promptics/jobagent/interviewplan/agents/WorkTopicsPlanningAgentTest.java
+++ src/test/java/io/promptics/jobagent/interviewplan/agents/WorkTopicsPlanningAgentTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -71,5 +72,6 @@ class WorkTopicsPlanningAgentTest {
                 """, new TypeReference<List<Work>>() {
         });
         List<Topic> topics = agent.plan(works);
+        assertThat(topics).isNotEmpty();
     }
 }
